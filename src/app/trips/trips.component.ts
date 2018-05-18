0766552f0fe44ad816950c9df94857856beb739b
TeddyStudent <teukka@bastu.net>
2018-05-19 00:13:33 +0300

    trips component modified

--- src/app/trips/trips.component.ts
+++ src/app/trips/trips.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TripsComponent implements OnInit {
 
+  reissut = [
+    { id: 1, pvm: '12-05-2017', paikka: 'Roine', saa: 'Aurinkoinen', tn: 1, ts: 'itä', li: 22, lv: 19 },
+    { id: 2, pvm: '11-06-2017', paikka: 'Saimaa', saa: 'Pilvinen', tn: 5, ts: 'etelä', li: 18, lv: 17 },
+    { id: 3, pvm: '11-06-2017', paikka: 'Saimaa', saa: 'Pilvinen', tn: 4, ts: 'etelä', li: 19, lv: 17 }
+  ];
+
+
   constructor() { }
 
   ngOnInit() {
